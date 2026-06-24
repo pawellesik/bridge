8d2a5afe9ca4e7bafbdf8eac3b19835f9d48556b
plesik <pawel.lesik@opi.org.pl>
2026-06-24 14:00:55 +0200

    Revert "dom s"
    This reverts commit 6d9e2d80

--- app/src/main/java/com/example/bridge/model/Suit.java
+++ app/src/main/java/com/example/bridge/model/Suit.java
@@ -3,25 +3,23 @@ package com.example.bridge.model;
 import com.example.bridge.R;
 
 public enum Suit {
-    SPADES(0, R.drawable.spades, false, "♠", 0xFF243D65, "black"),
-    HEARTS(1, R.drawable.heart, true, "♥", 0xFFC94B4B, "red"),
-    CLUBS(2, R.drawable.clubs, false, "♣", 0xFF388E3C, "#1976D2"),
-    DIAMONDS(3, R.drawable.diamonds, true, "♦", 0xFFF57C00, "#F57C00");
+    SPADES(0, R.drawable.spades, false, "♠", 0xFF243D65),
+    HEARTS(1, R.drawable.heart, true, "♥", 0xFFC94B4B),
+    CLUBS(2, R.drawable.clubs, false, "♣", 0xFF388E3C),
+    DIAMONDS(3, R.drawable.diamonds, true, "♦", 0xFFF57C00);
 
     public final int priority;
     public final int resId;
     public final boolean isRed;
     public final String symbol;
     public final int color;
-    public final String hexColor;
 
-    Suit(int priority, int resId, boolean isRed, String symbol, int color, String hexColor) {
+    Suit(int priority, int resId, boolean isRed, String symbol, int color) {
         this.priority = priority;
         this.resId = resId;
         this.isRed = isRed;
         this.symbol = symbol;
         this.color = color;
-        this.hexColor = hexColor;
     }
 
     public static Suit getSuit(String colorName) {
