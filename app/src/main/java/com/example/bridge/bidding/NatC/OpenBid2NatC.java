9e30575e8dced732ef00eb5ce779bb2da865b1b6
plesik <pawel.lesik.85@gmail.com>
2026-07-13 22:00:30 +0200

    dom s

--- app/src/main/java/com/example/bridge/bidding/NatC/OpenBid2NatC.java
+++ app/src/main/java/com/example/bridge/bidding/NatC/OpenBid2NatC.java
@@ -108,6 +108,9 @@ public class OpenBid2NatC extends OpenNatC {
                 shows(Bid._3H, FIT_8_PLUS, pairPoints(PAIR_GAME_INVITE), id("OpenBid2NatC.responderRaisedMajor _3H")),
                 shows(Bid._3S, FIT_8_PLUS, pairPoints(PAIR_GAME_INVITE), id("OpenBid2NatC.responderRaisedMajor _3S")),
 
+                shows(Bid._3H, shape(6,10), pairPoints(PAIR_GAME_INVITE), id("OpenBid2NatC.responderRaisedMajor _3H")),
+                shows(Bid._3S, shape(6,10), pairPoints(PAIR_GAME_INVITE), id("OpenBid2NatC.responderRaisedMajor _3S")),
+
                 shows(Bid._2H, FIT_8_PLUS, pairHighCardPoints(PAIR_LOW_GAME), id("OpenBid2NatC.responderRaisedMajor _2H")),
                 shows(Bid._2S, FIT_8_PLUS, pairHighCardPoints(PAIR_LOW_GAME), id("OpenBid2NatC.responderRaisedMajor _2S")),
 
