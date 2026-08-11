4d06573490cbe5fadacb1e448b5cebad51059804
plesik <pawel.lesik.85@gmail.com>
2026-08-11 10:18:38 +0200

    dom s

--- app/src/main/java/com/example/bridge/core/SharedPref.java
+++ app/src/main/java/com/example/bridge/core/SharedPref.java
@@ -20,30 +20,12 @@ public class SharedPref {
     public static final String PREFS_NAME = "BridgePrefs";
     static final String KEY_CAREER_IMP = "careerImp";
     static final String KEY_GAMES_PLAYED = "gamesPlayed";
-    static final String KEY_HAS_SAVED_DEAL = "hasSavedDeal";
-    public static final String KEY_HISTORY = "gameHistory";
     GameActivity gameActivity;
 
     public SharedPref(GameActivity gameActivity) {
         this.gameActivity = gameActivity;
     }
 
-
-    private int getContractPkt(Suit suit, int cnt) {
-        if (suit == null) {
-            return (cnt > 0) ? (cnt * 30 + 10) : 0;
-        }
-        switch (suit) {
-            case SPADES:
-            case HEARTS:
-                return cnt * 30;
-            case CLUBS:
-            case DIAMONDS:
-                return cnt * 20;
-            default:
-                return 0;
-        }
-    }
 
     public int getGamesPlayed() {
         SharedPreferences prefs = gameActivity.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
@@ -56,15 +38,6 @@ public class SharedPref {
         prefs.edit().putInt(KEY_GAMES_PLAYED, count).apply();
     }
 
-    public void saveGameHistory(String historyJson) {
-        SharedPreferences prefs = gameActivity.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        prefs.edit().putString(KEY_HISTORY, historyJson).apply();
-    }
-
-    public String getGameHistory() {
-        SharedPreferences prefs = gameActivity.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        return prefs.getString(KEY_HISTORY, "");
-    }
 
 
 }
