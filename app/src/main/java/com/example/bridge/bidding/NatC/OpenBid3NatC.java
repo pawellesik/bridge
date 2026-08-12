861f91e209580f9a47b6c8b42b246ae384145752
plesik <pawel.lesik@opi.org.pl>
2026-08-12 10:17:41 +0200

    bridge

--- app/src/main/java/com/example/bridge/bidding/NatC/OpenBid3NatC.java
+++ app/src/main/java/com/example/bridge/bidding/NatC/OpenBid3NatC.java
@@ -13,11 +13,13 @@ public class OpenBid3NatC extends OpenNatC {
     public static Iterable<CallFeature> thirdBid(PositionState ps) {
         List<CallFeature> bids = new ArrayList<>();
         bids.add(shows(Bid._1NT, BALANCED, points(Rebid1NT), id("OpenBid3NatC.thirdBid _1NT")));
-        bids.add(shows(Bid._2NT, BALANCED, points(Rebid2NT), id("OpenBid3NatC.thirdBid _2NT")));
+        bids.add(shows(Bid._2NT, PAIR_BALANCED, points(Rebid2NT), id("OpenBid3NatC.thirdBid _2NT")));
 
         bids.add(shows(Bid._4S, fit(), OpeningStrongBidding, id("OpenBid3NatC.thirdBid _4S")));
         bids.add(shows(Bid._4H, fit(), OpeningStrongBidding, id("OpenBid3NatC.thirdBid _4H")));
-        bids.add(shows(Bid._3NT, BALANCED, pairHighCardPoints(PAIR_GAME), id("OpenBid3NatC.thirdBid _3NT")));
+
+        bids.add(shows(Bid._3NT, PAIR_BALANCED, pairHighCardPoints(PAIR_GAME), id("OpenBid3NatC.thirdBid _3NT")));
+        bids.add(shows(Bid._3NT, PAIR_BALANCED, OpeningStrongBidding, shape(ps.getPartner().getBid().getSuit(),0,1), id("OpenBid3NatC.thirdBid _3NT")));
 
         bids.add(shows(Call.PASS, partner(isJump(1)), OpenBidding));
 
