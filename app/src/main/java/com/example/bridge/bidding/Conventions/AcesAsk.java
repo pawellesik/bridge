09095f693afe6cfc608b4073d0bc3f11cf1053fc
plesik <pawel.lesik@opi.org.pl>
2026-08-12 10:32:34 +0200

    bridge

--- app/src/main/java/com/example/bridge/bidding/Conventions/AcesAsk.java
+++ app/src/main/java/com/example/bridge/bidding/Conventions/AcesAsk.java
@@ -28,6 +28,7 @@ public class AcesAsk extends Bidder {
         bids.add(properties(Bid._4C, AcesAsk::respondCountAces, true, true, false, ps.getPartner().getBid().getSuit(), null, null, UserText.AcesAsc, null));
         bids.add(shows(Bid._4C, fit(ps.getPartner().getBid().getSuit()), IS_ANY_JUMP, points(ASK_ACES), pairHighCardPoints(HIGHT_GAME), id(" initiateConventionAcesAsk 1")));
         bids.add(shows(Bid._4C, fit(ps.getPartner().getBid().getSuit()), hasMultipleShortness(2, 0, 1), secondSuit(ps.getPartner().getBid().getSuit(), 5), points(15,30), id("initiateConvention AcesAsk 2")));
+        bids.add(shows(Bid._4C, IS_ANY_JUMP, pairHighCardPoints(SLAM_OR_BETTER) , id("initiateConvention AcesAsk 3")));
 
         return bids;
     }
