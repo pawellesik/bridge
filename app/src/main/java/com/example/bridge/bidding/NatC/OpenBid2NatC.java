520576b9c954039a55aa20827eb931a68791a901
plesik <pawel.lesik.85@gmail.com>
2026-08-16 19:16:54 +0200

    dom s

--- app/src/main/java/com/example/bridge/bidding/NatC/OpenBid2NatC.java
+++ app/src/main/java/com/example/bridge/bidding/NatC/OpenBid2NatC.java
@@ -82,7 +82,7 @@ public class OpenBid2NatC extends OpenNatC {
         return choices;
     }
 
-    public static PositionCalls responderRaiseChangedSuitsMinorDiamond(PositionState ps) {//todo
+    public static PositionCalls responderRaiseChangedSuitsMinorDiamond(PositionState ps) {
         //1D ->
         //     Bid._3C->
         PositionCalls choices = new PositionCalls(ps);
@@ -100,7 +100,7 @@ public class OpenBid2NatC extends OpenNatC {
         return choices;
     }
 
-    public static PositionCalls responder1NTDiamond(PositionState ps) {//todo
+    public static PositionCalls responder1NTDiamond(PositionState ps) {/
         //1D ->
         //     Bid._1NT->
         PositionCalls choices = new PositionCalls(ps);
@@ -116,7 +116,7 @@ public class OpenBid2NatC extends OpenNatC {
         return choices;
     }
 
-    public static PositionCalls responder2NTDiamond(PositionState ps) {//todo
+    public static PositionCalls responder2NTDiamond(PositionState ps) {
         //1D ->
         //     Bid._2NT->
         PositionCalls choices = new PositionCalls(ps);
