6b6e7e571752563109d90fe41dc2a78abb25988b
plesik <pawel.lesik.85@gmail.com>
2026-08-17 17:35:07 +0200

    dom s

--- app/src/main/java/com/example/bridge/bidding/NatC/RecursionNatC.java
+++ app/src/main/java/com/example/bridge/bidding/NatC/RecursionNatC.java
@@ -16,38 +16,46 @@ import java.util.List;
 public class RecursionNatC extends NatC {
 
     public static PositionCalls recursionBids(PositionState ps) {
+        return recursionBids(ps, 1);
+    }
+
+    public static PositionCalls recursionBids(PositionState ps, int level) {
         PositionCalls choices = new PositionCalls(ps);
-        choices.addRules(CompeteNatC.compBids(ps));
-        choices.addRules(
-                shows(Call.PASS, CONTRACT_IS_AGREED_STRAIN, id("RecursionNatC.recursionBids CONTRACT_IS_AGREED_STRAIN _Pass")),
+        if (level < 10) {
+            choices.addRules(CompeteNatC.compBids(ps));
+            choices.addRules(
+                    shows(Call.PASS, CONTRACT_IS_AGREED_STRAIN, id("RecursionNatC.recursionBids CONTRACT_IS_AGREED_STRAIN _Pass")),
 
-                shows(Bid._2H, shape(6, 10), IS_REBID, id("RecursionNatC.recursionBids IS_REBID _1H")),
-                shows(Bid._2S, shape(6, 10), IS_REBID, id("RecursionNatC.recursionBids IS_REBID _1S")),
-                shows(Bid._2D, shape(6, 10), IS_REBID, id("RecursionNatC.recursionBids IS_REBID _2D")),
-                shows(Bid._3C, shape(6, 10), IS_REBID, id("RecursionNatC.recursionBids IS_REBID _1H")),
-                shows(Bid._3D, shape(6, 10), IS_REBID, id("RecursionNatC.recursionBids IS_REBID _1S")),
-                shows(Bid._3H, shape(6, 10), IS_REBID, id("RecursionNatC.recursionBids IS_REBID _2D")),
-                shows(Bid._3S, shape(6, 10), IS_REBID, id("RecursionNatC.recursionBids IS_REBID _2D")),
+                    shows(Bid._2H, shape(6, 10), IS_REBID, id("RecursionNatC.recursionBids IS_REBID _1H")),
+                    shows(Bid._2S, shape(6, 10), IS_REBID, id("RecursionNatC.recursionBids IS_REBID _1S")),
+                    shows(Bid._2D, shape(6, 10), IS_REBID, id("RecursionNatC.recursionBids IS_REBID _2D")),
+                    shows(Bid._3C, shape(6, 10), IS_REBID, id("RecursionNatC.recursionBids IS_REBID _1H")),
+                    shows(Bid._3D, shape(6, 10), IS_REBID, id("RecursionNatC.recursionBids IS_REBID _1S")),
+                    shows(Bid._3H, shape(6, 10), IS_REBID, id("RecursionNatC.recursionBids IS_REBID _2D")),
+                    shows(Bid._3S, shape(6, 10), IS_REBID, id("RecursionNatC.recursionBids IS_REBID _2D")),
 
-                shows(Bid._2H, shape(5, 10), IS_NEW_SUIT, id("RecursionNatC.recursionBids IS_REBID _1H")),
-                shows(Bid._2S, shape(5, 10), IS_NEW_SUIT, id("RecursionNatC.recursionBids IS_REBID _1S")),
-                shows(Bid._2C, shape(5, 10), IS_NEW_SUIT, id("RecursionNatC.recursionBids IS_REBID _1H")),
-                shows(Bid._2D, shape(5, 10), IS_NEW_SUIT, id("RecursionNatC.recursionBids IS_REBID _1S")),
-                shows(Bid._3H, shape(5, 10), IS_NEW_SUIT, id("RecursionNatC.recursionBids IS_REBID _1H")),
-                shows(Bid._3S, shape(5, 10), IS_NEW_SUIT, id("RecursionNatC.recursionBids IS_REBID _1S")),
-                shows(Bid._3C, shape(5, 10), IS_NEW_SUIT, id("RecursionNatC.recursionBids IS_REBID _1H")),
-                shows(Bid._3D, shape(5, 10), IS_NEW_SUIT, id("RecursionNatC.recursionBids IS_REBID _1S")),
+                    shows(Bid._2H, shape(5, 10), IS_NEW_SUIT, id("RecursionNatC.recursionBids IS_REBID _1H")),
+                    shows(Bid._2S, shape(5, 10), IS_NEW_SUIT, id("RecursionNatC.recursionBids IS_REBID _1S")),
+                    shows(Bid._2C, shape(5, 10), IS_NEW_SUIT, id("RecursionNatC.recursionBids IS_REBID _1H")),
+                    shows(Bid._2D, shape(5, 10), IS_NEW_SUIT, id("RecursionNatC.recursionBids IS_REBID _1S")),
+                    shows(Bid._3H, shape(5, 10), IS_NEW_SUIT, id("RecursionNatC.recursionBids IS_REBID _1H")),
+                    shows(Bid._3S, shape(5, 10), IS_NEW_SUIT, id("RecursionNatC.recursionBids IS_REBID _1S")),
+                    shows(Bid._3C, shape(5, 10), IS_NEW_SUIT, id("RecursionNatC.recursionBids IS_REBID _1H")),
+                    shows(Bid._3D, shape(5, 10), IS_NEW_SUIT, id("RecursionNatC.recursionBids IS_REBID _1S")),
 
-                shows(Bid._2H, shape(4, 10), IS_NEW_SUIT, DECENT_PLUS_SUIT, id("RecursionNatC.recursionBids IS_REBID _1H")),
-                shows(Bid._2S, shape(4, 10), IS_NEW_SUIT, DECENT_PLUS_SUIT, id("RecursionNatC.recursionBids IS_REBID _1S")),
-                shows(Bid._3H, shape(4, 10), IS_NEW_SUIT, DECENT_PLUS_SUIT, id("RecursionNatC.recursionBids IS_REBID _1H")),
-                shows(Bid._3S, shape(4, 10), IS_NEW_SUIT, DECENT_PLUS_SUIT, id("RecursionNatC.recursionBids IS_REBID _1S")),
+                    shows(Bid._2H, shape(4, 10), IS_NEW_SUIT, DECENT_PLUS_SUIT, id("RecursionNatC.recursionBids IS_REBID _1H")),
+                    shows(Bid._2S, shape(4, 10), IS_NEW_SUIT, DECENT_PLUS_SUIT, id("RecursionNatC.recursionBids IS_REBID _1S")),
+                    shows(Bid._3H, shape(4, 10), IS_NEW_SUIT, DECENT_PLUS_SUIT, id("RecursionNatC.recursionBids IS_REBID _1H")),
+                    shows(Bid._3S, shape(4, 10), IS_NEW_SUIT, DECENT_PLUS_SUIT, id("RecursionNatC.recursionBids IS_REBID _1S")),
 
-                shows(Bid._1NT, PAIR_BALANCED, id("RecursionNatC.recursionBids IS_REBID _2D")),
-                shows(Bid._2NT, PAIR_BALANCED, id("RecursionNatC.recursionBids IS_REBID _2D")),
-                shows(Bid._3NT, PAIR_BALANCED, id("RecursionNatC.recursionBids IS_REBID _2D"))
-        );
-        choices.addRules(RecursionNatC.recursionBids(ps));
+                    shows(Bid._1NT, PAIR_BALANCED, id("RecursionNatC.recursionBids IS_REBID _2D")),
+                    shows(Bid._2NT, PAIR_BALANCED, id("RecursionNatC.recursionBids IS_REBID _2D")),
+                    shows(Bid._3NT, PAIR_BALANCED, id("RecursionNatC.recursionBids IS_REBID _2D"))
+            );
+            choices.addRules(RecursionNatC.recursionBids(ps, level + 1));
+        } else {
+            choices.addRules(shows(Call.PASS, id("RecursionNatC.recursionBids Pass")));
+        }
         return choices;
     }
 
