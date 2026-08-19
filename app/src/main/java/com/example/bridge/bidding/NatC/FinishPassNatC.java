9618b22be3d0bb01e3aa897c0a458f67684e0e52
plesik <pawel.lesik@opi.org.pl>
2026-08-19 15:15:32 +0200

    bridge

---
+++ app/src/main/java/com/example/bridge/bidding/NatC/FinishPassNatC.java
@@ -0,0 +1,36 @@
+package com.example.bridge.bidding.NatC;
+
+import com.example.bridge.bidding.Constraints.Shape;
+import com.example.bridge.bidding.Conventions.AcesAsk;
+import com.example.bridge.bidding.Tools.Bid;
+import com.example.bridge.bidding.Tools.Call;
+import com.example.bridge.bidding.Tools.CallFeature;
+import com.example.bridge.bidding.Tools.PositionState;
+import com.example.bridge.bidding.Tools.Suit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FinishPassNatC extends NatC {
+
+
+    public static Iterable<CallFeature> finishBids(PositionState ps) {
+        List<CallFeature> bids = new ArrayList<>();
+
+        bids.add(shows(Call.PASS, CONTRACT_IS_AGREED_STRAIN, pairHighCardPoints(PAIR_LOW_GAME), partner(isLastBid(Bid._2H, Bid._2S)), id("FinishPassNatC.finishBids pass")));
+        bids.add(shows(Call.PASS, CONTRACT_IS_AGREED_STRAIN, pairHighCardPoints(PAIR_LOW_GAME), partner(isLastBid(Bid._3C, Bid._3D)), id("FinishPassNatC.finishBids pass")));
+        bids.add(shows(Call.PASS, CONTRACT_IS_AGREED_STRAIN, pairHighCardPoints(PAIR_GAME), partner(isLastBid(Bid._4H, Bid._5S)), id("FinishPassNatC.finishBids pass")));
+        bids.add(shows(Call.PASS, CONTRACT_IS_AGREED_STRAIN, pairHighCardPoints(PAIR_MINOR_GAME), partner(isLastBid(Bid._5C, Bid._5D)), id("FinishPassNatC.finishBids pass")));
+
+        bids.add(shows(Call.PASS, pairHighCardPoints(PAIR_GAME), partner(isLastBid(Bid._3NT)), id("FinishPassNatC.finishBids pass")));
+
+
+        return bids;
+    }
+}
+
+
+
+
+
+
--- app/src/main/java/com/example/bridge/bidding/NatC/OpenBid2NatC.java
+++ app/src/main/java/com/example/bridge/bidding/NatC/OpenBid2NatC.java
@@ -381,13 +381,12 @@ public class OpenBid2NatC extends OpenNatC {
         PositionCalls choices = new PositionCalls(ps);
         choices.addRules(AcesAsk.initiateConvention(ps));
         choices.addRules(AcesAsk.initiateConventionBlok(ps));
-        //System.out.println("plesik "+ps.getPartner().getPublicHandSummary().getHighCardPoints());
-
+        choices.addRules(FinishPassNatC.finishBids(ps));
         choices.addRules(
                 properties(new Call[]{Bid._2H, Bid._2S, Bid._3H, Bid._3S}, RespondBid2NatC::secondBidMinorAgreeTrumpDiamods),
                 partnerBids(NatC::finishBiddingIterable),
                 //shows(Call.PASS, CONTRACT_IS_AGREED_STRAIN, OpeningLowBidding, partner(isLastBid(Bid._2D)), id("OpenBid2NatC.responderdTrumpMinorDiamod pass")),
-                shows(Call.PASS, CONTRACT_IS_AGREED_STRAIN, pairHighCardPoints(PAIR_LOW_GAME), partner(isLastBid(Bid._3D)), id("OpenBid2NatC.responderdTrumpMinorDiamod pass")),
+                //shows(Call.PASS, CONTRACT_IS_AGREED_STRAIN, pairHighCardPoints(PAIR_LOW_GAME), partner(isLastBid(Bid._3D)), id("OpenBid2NatC.responderdTrumpMinorDiamod pass")),
 
                 shows(Bid._2H, shape(4, 10), DECENT_PLUS_SUIT, id("OpenBid2NatC.responderdTrumpMinorDiamod 2H")),
                 shows(Bid._2S, shape(4, 10), DECENT_PLUS_SUIT, id("OpenBid2NatC.responderdTrumpMinorDiamod 2S")),
