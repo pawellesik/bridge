614b66ad2959124f49606dbae6e9c5b1204d0266
plesik <pawel.lesik@opi.org.pl>
2026-08-20 13:30:00 +0200

    bridge

--- app/src/main/java/com/example/bridge/bidding/NatC/RespondBid2NatC.java
+++ app/src/main/java/com/example/bridge/bidding/NatC/RespondBid2NatC.java
@@ -45,7 +45,7 @@ public class RespondBid2NatC extends RespondNatC {
                 shows(Bid._4H, fit(), pairPoints(PAIR_GAME), id("RespondBid2NatC.secondBidNegatStrong _4H")),
                 shows(Bid._2S, noFit(), shape(5, 10), id("RespondBid2NatC.secondBidNegatStrong _2S")),
                 shows(Bid._3H, noFit(), shape(6, 10), id("RespondBid2NatC.secondBidNegatStrong _3H")),
-                shows(Bid._3H, shape(5, 10), shape(ps.getPartner().getBid().getSuit(), 0, 1), id("RespondBid2NatC.secondBidNegatStrong _3H")),
+                shows(Bid._3H, shape(5, 10), noFit(), shape(ps.getPartner().getBid().getSuit(), 0, 1), id("RespondBid2NatC.secondBidNegatStrong _3H")),
                 shows(Bid._3C, noFit(), shape(5, 10), id("RespondBid2NatC.secondBidNegatStrong _3C")),
                 shows(Bid._3D, noFit(), shape(5, 10), id("RespondBid2NatC.secondBidNegatStrong _3D"))
         );
