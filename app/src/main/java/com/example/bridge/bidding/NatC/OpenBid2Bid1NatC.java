ae0d96703a874060d3d5240a6e2a9a625866473c
plesik <pawel.lesik.85@gmail.com>
2026-09-12 14:46:57 +0200

    responderdTrumpMajorClubStrong

--- app/src/main/java/com/example/bridge/bidding/NatC/OpenBid2Bid1NatC.java
+++ app/src/main/java/com/example/bridge/bidding/NatC/OpenBid2Bid1NatC.java
@@ -84,8 +84,9 @@ public class OpenBid2Bid1NatC extends OpenBid1NatC {
                 shows(Bid._3S, OpeningStrongBidding, fit(), isJump(1), setTrumpColor(Suit.Spades), ruleShow(1), id("OpenBid2NatC.responderdTrumpMajorClubStrong _3S")),
 
                 shows(Bid._2S, OpeningStrongBidding, shape(5, 10), isJump(1), ruleShow(1), id("OpenBid2NatC.responderdTrumpMajorClubStrong _2S")),
-                shows(Bid._3C, OpeningStrongBidding, shape(5, 10), isJump(1), ruleShow(1), id("OpenBid2NatC.responderdTrumpMajorClubStrong _3C")),
-                shows(Bid._3D, OpeningStrongBidding, shape(5, 10), isJump(1), ruleShow(1), id("OpenBid2NatC.responderdTrumpMajorClubStrong _3D")),
+
+                shows(Bid._3C, OpeningStrongBidding, noFit(), shape(5, 10), isJump(1), ruleShow(1), id("OpenBid2NatC.responderdTrumpMajorClubStrong _3C")),
+                shows(Bid._3D, OpeningStrongBidding, noFit(), shape(5, 10), isJump(1), ruleShow(1), id("OpenBid2NatC.responderdTrumpMajorClubStrong _3D")),
 
                 shows(Bid._4H, OpeningStrongBidding, shape(6, 10), ruleShow(1), id("OpenBid2NatC.responderdTrumpMajorClubStrong _4H")),
                 shows(Bid._4S, OpeningStrongBidding, shape(6, 10), ruleShow(1), id("OpenBid2NatC.responderdTrumpMajorClubStrong _4S")),
